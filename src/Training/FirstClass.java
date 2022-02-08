0a21f804c991dac4a0f7a939a16a60e5a37c8f2f
chand <chand@DESKTOP-2G6SFJH>
2022-02-08 18:49:53 -0500

    upload all project files

---
+++ src/Training/FirstClass.java
@@ -0,0 +1,17 @@
+package Training;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		int x = 40;
+		int y = 50;
+		
+		System.out.println("Welcome to the world of JAVA programming");
+		
+		System.out.println(x * y);
+		
+		
+	}
+
+}
